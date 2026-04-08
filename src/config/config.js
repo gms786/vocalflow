45255e0b1e387d6ac27689f4e01432e12c31c1e3
Gaushmohammad Siddiqui <gmsiddiqui36@gmail.com>
2026-04-08 09:05:30 +0530

    Added dist

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,5 @@
-export const DEEPGRAM_API_KEY = process.env.REACT_APP_DEEPGRAM_API_KEY;
-export const GROQ_API_KEY = process.env.REACT_APP_GROQ_API_KEY;
+// export const DEEPGRAM_API_KEY = process.env.REACT_APP_DEEPGRAM_API_KEY;
+// export const GROQ_API_KEY = process.env.REACT_APP_GROQ_API_KEY;
+
+export const DEEPGRAM_API_KEY = "a93b00f3bea7b07336d7a5305784c4da3479475a";
+export const GROQ_API_KEY = "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";
